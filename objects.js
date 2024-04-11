a644e53048f13fc388baa0b99c477e27bb8ca53f
daniloBraj <165780340+daniloBraj@users.noreply.github.com>
2024-04-11 17:28:42 +0100

    changed formatting of challenes.js

--- objects.js
+++ objects.js
@@ -1,35 +1,34 @@
 const person = {
     name: "Danilo",
-    age: 21
-
+    age: 21,
+    greeting() {
+        return `Hi I'm ${this.name}`
+    }
 }
 
 person.bands = [
     "System of a Down",
     "Charli XCX",
     "Paramore",
-    "Massive Attack"
+    "Massive Attack",
 ]
 
-console.log(person.name)//use dot notation to access the keys of an object
-console.log(person["age"])//same goes for square brackets. use "" for syntax
-console.log(person)
-
+console.log(person);
 //
 
-const alarmClock = {
-    weekdayAlarm: "Wake up at 7 AM",
-    weekendAlarm: "No alarm set"
-}
-let day = "Sunday"
-let alarm
-    if(day == "Saturday" || day == "Sunday"){
-    alarm = alarmClock.weekendAlarm
-}
-    else{
-    alarm = alarmClock.weekdayAlarm
-}
-console.log(alarm)
+// const alarmClock = {
+//     weekdayAlarm: "Wake up at 7 AM",
+//     weekendAlarm: "No alarm set"
+// }
+// let day = "Sunday"
+// let alarm
+//     if(day == "Saturday" || day == "Sunday"){
+//     alarm = alarmClock.weekendAlarm
+// }
+//     else{
+//     alarm = alarmClock.weekdayAlarm
+// }
+// console.log(alarm)
 
 
 //
